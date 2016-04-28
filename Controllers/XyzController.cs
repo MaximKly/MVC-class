4837575ddab1aad0db6aab067897e03127bab2a3
MaximKly <alexey-k@rogers.com>
2016-04-28 08:09:48 -0400

    Added files via upload

---
+++ Controllers/XyzController.cs
@@ -0,0 +1,57 @@
+﻿using LearningASPMVC.Models;
+using LearningASPMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningASPMVC.Controllers
+{
+    public class XyzController : Controller
+    {
+        // GET: Xyz
+        private SchoolContext db = new SchoolContext();
+        public ActionResult Abc()
+        {
+            var students = db.Students.ToList();
+            return View(students);
+            //Course math = new Course();
+            //math.CourseName = "Math 101";
+            //math.TotalCredits = 4;
+
+            //Student alex = new Student();
+            //alex.FirstName = "Alex";
+            //alex.LastName = "Rod";
+
+            //Student lynda = new Student();
+            //lynda.FirstName = "Lynda";
+            //lynda.LastName = "Berry";
+
+            //Student john = new Student();
+            //john.FirstName = "John";
+            //john.LastName = "Doe";
+
+            //List<Student> students = new List<Student>();
+            //students.Add(alex);
+            //students.Add(lynda);
+            //students.Add(john);
+
+
+            //CourseStudents obj = new CourseStudents();
+            //obj.course = math;
+            //obj.students = students;
+           
+            //return View(alex);
+            //return "From XyzController, Abc action method";
+            //return View(obj);
+            
+        }
+
+        public ActionResult Index()
+        {
+             return View();
+            //return "From XyzController, Index action method";
+        }
+    }
+}
